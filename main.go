73ddd4a354ef8d75981fb9cea4102db3753b3068
daniel chapko <danielchapko@gmail.com>
2026-01-04 15:43:41 -0800

    basic submit form

--- main.go
+++ main.go
@@ -12,6 +12,12 @@ func main() {
 	http.HandleFunc("/clicked", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "good job you clicked")
 	})
+
+	http.HandleFunc("/message", func(w http.ResponseWriter, r *http.Request) {
+		text := r.FormValue("userInput")
+		fmt.Println("Received message:", text)
+		fmt.Fprintf(w, "You Sent: %s", text)
+	})
 	fmt.Println("started server")
 	http.ListenAndServe(":8080", nil)
 }
